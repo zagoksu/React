a0519347ae49b6684c465067cb333c6784c69c68
zagoksu <zeynelgoksu@gmail.com>
2020-06-14 11:37:38 +0200

    week3 files deleted

--- week3/homework/hackyourweather/src/Components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button({ onSubmit, isButtonDisabled }) {
-  return (
-    <button className="searchButton" onClick={onSubmit} type="submit" disabled={isButtonDisabled}>
-      Search
-    </button>
-  );
-}
